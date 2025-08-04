cc91d2d5056403db688e6163a3aa8ebc20768d68
Jelly Maweja <jellymaweja7@gmail.com>
2025-08-04 14:22:05 +0100

    abrutis

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path/win32'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()], build: {
     target: 'es2020'  // Assure la compatibilité
-  }
+  },  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
