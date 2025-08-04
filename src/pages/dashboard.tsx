60a0dcd18564d0d188ca169e06b263dbb71e61f6
JMN <119761722+JellyMnavita@users.noreply.github.com>
2025-08-04 14:57:17 +0100

    Add files via upload

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,19 @@
+
+import { OverviewAnalyticsView as DashboardView } from '../sections/overview/view';
+
+// ----------------------------------------------------------------------
+
+export default function Page() {
+  return (
+    <>
+      <title>{`Dashboard - SafiMayi`}</title>
+      <meta
+        name="description"
+        content="The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style"
+      />
+      <meta name="keywords" content="react,material,kit,application,dashboard,admin,template" />
+
+      <DashboardView />
+    </>
+  );
+}
