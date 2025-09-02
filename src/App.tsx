a83f76e3e1a598a05ac1c5282a81aa53026bebd7
Jelly Maweja <jellymaweja7@gmail.com>
2025-09-02 14:33:40 +0100

    slsldksld

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,3 @@
-/* import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-
-const isAuthenticated = () => {
-  return localStorage.getItem('token') !== null;
-};
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={isAuthenticated() ? <Dashboard /> : <Navigate to="/login" />}
-        />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
- */
 import './global.css';
 import { useEffect } from 'react';
 import { usePathname } from './routes/hooks';
